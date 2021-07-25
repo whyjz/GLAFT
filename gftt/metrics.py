e794dffc4af1a5670980b773dbbdd8000f3b36b9
Whyjay Zheng <whyjz@berkeley.edu>
2021-07-25 19:34:35 +0000

    Finish metric_uncertainty.ipynb

--- gftt/metrics.py
+++ gftt/metrics.py
@@ -1,25 +1,85 @@
+import numpy as np
+from scipy.stats import gaussian_kde
 import geopandas as gpd
 from shapely.geometry import mapping
 import rasterio
 from rasterio.mask import mask
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
-def off_ice_errors(ft_results=None, off_ice_area=None):
+def off_ice_errors(vfile=None, vxfile=None, vyfile=None, off_ice_area=None, thres_sigma=3.0, plot=True, ax=None):
     """
-    ft_results: str, geotiff file path
+    vfile: str, geotiff file path
+    vxfile: str, geotiff file path
+    vyfile: str, geotiff file path
     off_ice_area: str, off ice area (shapefile) file path
     ----
-    return all pixel values within a given polygon shapefile.
+    returns:
+    vx: 1-d np array, vx values from all pixels within the off-ice area.
+    vy: 1-d np array, vy values from all pixels within the off-ice area.
+    z: 1-d np array (float), Gaussian KDE values for all pixels within the off-ice area.
+    thres_idx: 1-d np array (boolean), indices of pixels with a z value within a pre-defined confidence level. 
+               when thres_sigma=3.0, confidence level = 99.7%.
+    ==== or ====
+    v: 1-d np array, v values from all pixels within the off-ice area.
+    bins: plt.hist return with 100 bins and v^2 as input
+    ----
     according to
     https://gis.stackexchange.com/questions/260304/extract-raster-values-within-shapefile-with-pygeoprocessing-or-gdal
     """  
     shapefile = gpd.read_file(off_ice_area)
     geoms = shapefile.geometry.values
     geoms = [mapping(geoms[i]) for i in range(len(geoms))]
-    with rasterio.open(ft_results) as src:
-        out_image, out_transform = mask(src, geoms, crop=True, nodata=-9999.0)
-    try:
-        clipped_data = out_image.data[0]
-    except NotImplementedError:
-        clipped_data = out_image[0]
-    return clipped_data
+    
+    def clip(gtiff, geoms):
+        with rasterio.open(gtiff) as src:
+            out_image, out_transform = mask(src, geoms, crop=True, nodata=-9999.0)
+        try:
+            clipped_data = out_image.data[0]
+        except NotImplementedError:
+            clipped_data = out_image[0]
+        return clipped_data
 
+    if vxfile is not None and vyfile is not None:
+        case = 1
+        vx = clip(vxfile, geoms)
+        vy = clip(vyfile, geoms)
+        vx = vx[vx > -9998]  # remove NaN points
+        vy = vy[vy > -9998]  # remove NaN points
+    elif vfile is not None:
+        case = 2
+        v = clip(vfile, geoms)
+        v = v[v > -9998]  # remove NaN points
+    else:
+        case = 0
+        raise TypeError('Either vfile or vxfile+vyfile are required.')
+        
+    if case == 1:
+        xy = np.vstack([vx, vy])
+        z = gaussian_kde(xy)(xy)
+
+        thres_multiplier = np.e ** (thres_sigma ** 2 / 2)   # normal dist., +- sigma number 
+        thres = max(z) / thres_multiplier
+        thres_idx = z >= thres
+        idx = thres_idx    # alias
+
+        if plot:
+            if ax is None:
+                fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+                
+            viridis = cm.get_cmap('viridis', 12)
+            pt_style = {'s': 6, 'edgecolor': None}
+            
+            ax.scatter(vx[idx], vy[idx], c=z[idx], **pt_style)
+            ax.scatter(vx[~idx], vy[~idx], color=viridis(0), alpha=0.4, **pt_style)
+            
+        return vx, vy, z, thres_idx
+    
+    elif case == 2:
+        
+        if plot:
+            if ax is None:
+                fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+                
+        bins = ax.hist(v ** 2, 100);
+        return v, bins
